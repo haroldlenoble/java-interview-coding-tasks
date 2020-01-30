dfa13df5deffb40807817807303a4eca738c1b93
harold <hibouanga@amiltone.com>
2020-01-30 11:19:33 +0100

    Fibonacci test both recursive and iterative algorithm

---
+++ src/main/java/com/coding/game/Fibonacci.java
@@ -0,0 +1,41 @@
+package com.coding.game;
+
+/**
+ * Fibonacci
+ */
+public class Fibonacci {
+
+    /**
+     * This algorithm can be slow and lead ti StackOverFlow error
+     * @param n
+     * @return
+     */
+    public static int recuFib(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException("The parameter must be >= 1.");
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        } else {
+            return recuFib(n - 1) + recuFib(n - 2);
+        }
+    }
+    /**
+     * This algorithm is more safe and fast than the first one
+     * @param n
+     * @return
+     */
+    public static int itFib(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException("The parameter must be >= 1.");
+        }
+        int n1 = 1, n2 = 1, n3 = 1;
+        for (int i = 2; i < n; i++) {
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+        }
+        return n3;
+    }
+
+}
---
+++ src/test/java/com/coding/game/AppTest.java
@@ -0,0 +1,26 @@
+package com.coding.game;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+
+    @Test
+    public void compare(){
+        assertEquals(Fibonacci.recuFib(1), Fibonacci.itFib(1));
+        assertEquals(Fibonacci.recuFib(2), Fibonacci.itFib(2));
+        assertEquals(Fibonacci.recuFib(3), Fibonacci.itFib(3));
+        assertEquals(Fibonacci.recuFib(4), Fibonacci.itFib(4));
+        assertEquals(Fibonacci.recuFib(5), Fibonacci.itFib(5));
+        assertEquals(Fibonacci.recuFib(6), Fibonacci.itFib(6));
+        assertEquals(Fibonacci.recuFib(7), Fibonacci.itFib(7));
+        assertEquals(Fibonacci.recuFib(8), Fibonacci.itFib(8));
+    }
+}
